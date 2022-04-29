79ef5beeab64202c7b981918aeaf2f8e16c6b336
George Saad <gsparqcode@gmail.com>
2022-04-29 19:26:49 -0400

    feature/potion and inventory

---
+++ __tests__/player.test.js
@@ -0,0 +1,19 @@
+const Player = require("../lib/Player");
+const Potion = require("../lib/Potion");
+
+jest.mock("../lib/Potion");
+console.log(new Potion());
+test("creates a player object", () => {
+  const player = new Player("Dave");
+
+  expect(player.name).toBe("Dave");
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+
+  expect(player.inventory).toEqual(
+    //expect inventory to equal an array containing any object!
+    //   inventory will be an object
+    expect.arrayContaining([expect.any(Object)])
+  );
+});
